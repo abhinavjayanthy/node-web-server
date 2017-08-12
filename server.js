4e2a51ef34e284d989cb59577084d92548b585c4
Abhinav Jayanthy <abhinavjayanthy@Abhinavs-MacBook-Pro.local>
2017-08-12 19:22:28 -0400

    Setup for heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
+
+const port = process.env.PORT || 3000;
 app.set('view engine','hbs')
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -50,6 +52,6 @@ app.get('/bad',(req,res) => {
 }
 )
 
-app.listen(3000,() => {
-    console.log('Server is up and running');
+app.listen(port,() => {
+    console.log(`Server is up and running at ${port}`);
 });
